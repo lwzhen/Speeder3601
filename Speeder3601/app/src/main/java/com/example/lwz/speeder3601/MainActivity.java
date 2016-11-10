53cb81c86ad0d2d90d15c3f5aa5214c7387d7d0d
lwz <liuwenzhen@asiatravel>
2016-11-10 10:59:13 +0800

    first second

---
+++ Speeder3601/app/src/main/java/com/example/lwz/speeder3601/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lwz.speeder3601;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //aa
+        setContentView(R.layout.activity_main);
+    }
+}
